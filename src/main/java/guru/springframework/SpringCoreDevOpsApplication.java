085725c42cb3b36a83115d588ee916664bdab8e5
Sridatta <sridatta.pasumarthy@talentica.com>
2017-03-17 14:19:58 +0530

    Second Commit

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -9,9 +9,7 @@ public class SpringCoreDevOpsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
-		//this is a whitespace change test
-		// another test...
-
 		//this is a whitespace change test to start jenkins build
+		//another test
 	}
 }
